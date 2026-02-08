40815cca56b1d460a09bbc8ae281b08350b92fc6
Muhammad Shafqat Rasool <mshafqat.developer@gmail.com>
2026-02-08 22:47:03 +0500

    exception handling

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -37,10 +37,10 @@ export default function Sidebar({
     if (isSearching) fetchUsers();
   }, [isSearching]);
 
-  const filteredUsers = users.filter(
+  const filteredUsers = users?.filter(
     (u) =>
-      u.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
-      u.email.toLowerCase().includes(searchQuery.toLowerCase()),
+      u.name?.toLowerCase().includes(searchQuery?.toLowerCase()) ||
+      u.email?.toLowerCase().includes(searchQuery?.toLowerCase()),
   );
 
   return (
